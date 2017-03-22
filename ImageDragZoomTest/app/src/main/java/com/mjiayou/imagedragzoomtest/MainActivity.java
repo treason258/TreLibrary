71a3002bedc4d89ca3b17a8880995fafe89ce47e
treason-rmbp <ma.tengfei2008@163.com>
2017-03-22 18:41:58 +0800

    ImageDragZoomTest - 来自github

--- ImageDragZoomTest/app/src/main/java/com/mjiayou/imagedragzoomtest/MainActivity.java
+++ ImageDragZoomTest/app/src/main/java/com/mjiayou/imagedragzoomtest/MainActivity.java
@@ -1,13 +1,33 @@
 package com.mjiayou.imagedragzoomtest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Proudly to use Intellij IDEA.
+ * Created by ay27 on 14-7-7.
+ */
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.main);
+
+
+        BottomImageView bottomImageView = (BottomImageView)findViewById(R.id.layout2_bottomView);
+        ImageView zoomArea = (ImageView)findViewById(R.id.layout2_imageAbove);
+        ChooseArea chooseArea = (ChooseArea)findViewById(R.id.layout2_topView);
+
+        bottomImageView.setZoomView(zoomArea);
+        chooseArea.setBottomView(bottomImageView);
+        chooseArea.setRegion(new Point(200, 400), new Point(600, 400), new Point(600, 900), new Point(200, 900));
+
     }
 }
