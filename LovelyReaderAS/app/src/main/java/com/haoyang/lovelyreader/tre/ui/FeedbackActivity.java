99458f13f7f9f1d2d7d61315d783e849b5ee363e
matengfei <matengfei@xin.com>
2018-10-08 17:16:21 +0800

    LovelyReaderAS - 意见反馈功能

--- LovelyReaderAS/app/src/main/java/com/haoyang/lovelyreader/tre/ui/FeedbackActivity.java
+++ LovelyReaderAS/app/src/main/java/com/haoyang/lovelyreader/tre/ui/FeedbackActivity.java
@@ -2,22 +2,29 @@ package com.haoyang.lovelyreader.tre.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.haoyang.lovelyreader.R;
 import com.haoyang.lovelyreader.tre.base.BaseActivity;
+import com.haoyang.lovelyreader.tre.bean.UserBean;
 import com.haoyang.lovelyreader.tre.bean.api.ApiRequest;
 import com.haoyang.lovelyreader.tre.bean.api.CommonData;
 import com.haoyang.lovelyreader.tre.bean.api.FeedbackAddParam;
+import com.haoyang.lovelyreader.tre.helper.DBHelper;
+import com.haoyang.lovelyreader.tre.helper.EncodeHelper;
 import com.haoyang.lovelyreader.tre.helper.UrlConfig;
 import com.mjiayou.trecorelib.http.RequestEntity;
 import com.mjiayou.trecorelib.http.RequestMethod;
 import com.mjiayou.trecorelib.http.okhttp.RequestBuilder;
 import com.mjiayou.trecorelib.http.okhttp.RequestCallback;
+import com.mjiayou.trecorelib.json.JsonHelper;
 import com.mjiayou.trecorelib.util.ToastUtils;
+import com.mjiayou.trecorelib.util.UserUtils;
 
 import java.util.ArrayList;
 
@@ -31,6 +38,10 @@ public class FeedbackActivity extends BaseActivity {
     private TextView tvTitle;
     private ImageView ivBack;
 
+    private EditText etProblem;
+    private EditText etPhone;
+    private TextView tvSubmit;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,9 +51,11 @@ public class FeedbackActivity extends BaseActivity {
         rlTitle = (RelativeLayout) findViewById(R.id.rlTitle);
         tvTitle = (TextView) findViewById(R.id.tvTitle);
         ivBack = (ImageView) findViewById(R.id.ivBack);
+        etProblem = (EditText) findViewById(R.id.etProblem);
+        etPhone = (EditText) findViewById(R.id.etPhone);
+        tvSubmit = (TextView) findViewById(R.id.tvSubmit);
 
         initView();
-        feedbackAdd();
     }
 
     @Override
@@ -57,38 +70,56 @@ public class FeedbackActivity extends BaseActivity {
                 finish();
             }
         });
-    }
 
-    /**
-     * 意见反馈
-     */
-    private void feedbackAdd() {
-        FeedbackAddParam feedbackAddParam = new FeedbackAddParam();
-        feedbackAddParam.setImgList(new ArrayList<String>());
-        feedbackAddParam.setPhone("");
-        feedbackAddParam.setProblemDesc("");
-        feedbackAddParam.setUid("");
-        feedbackAddParam.setUserName("");
-        ApiRequest apiRequest = new ApiRequest();
-        apiRequest.setCommonData(CommonData.get());
-        apiRequest.setParam(feedbackAddParam);
-
-        RequestEntity requestEntity = new RequestEntity(UrlConfig.apiFeedbackAdd);
-        requestEntity.setMethod(RequestMethod.POST_STRING);
-        requestEntity.setContent(apiRequest);
-        RequestBuilder.get().send(requestEntity, new RequestCallback<String>() {
+        // tvSubmit
+        tvSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onStart() {
-            }
+            public void onClick(View v) {
+                final String problem = etProblem.getText().toString();
+                final String phone = etPhone.getText().toString();
 
-            @Override
-            public void onSuccess(int code, String object) {
+                if (TextUtils.isEmpty(problem)) {
+                    ToastUtils.show("请描述您的问题");
+                    return;
+                }
+                if (TextUtils.isEmpty(phone)) {
+                    ToastUtils.show("请输入手机号码");
+                    return;
+                }
 
-            }
+                UserBean userBean = DBHelper.getUserBean();
+                FeedbackAddParam feedbackAddParam = new FeedbackAddParam();
+                feedbackAddParam.setImgList(new ArrayList<String>());
+                feedbackAddParam.setPhone(phone);
+                feedbackAddParam.setProblemDesc(problem);
+                feedbackAddParam.setUid(userBean.getUid());
+                feedbackAddParam.setUserName(userBean.getNickName());
+                ApiRequest apiRequest = new ApiRequest();
+                apiRequest.setCommonData(CommonData.get());
+                apiRequest.setParam(feedbackAddParam);
+                String content = JsonHelper.get().toJson(apiRequest);
 
-            @Override
-            public void onFailure(int code, String msg) {
-                ToastUtils.show(msg);
+                RequestEntity requestEntity = new RequestEntity(UrlConfig.apiFeedbackAdd);
+                requestEntity.setMethod(RequestMethod.POST_STRING);
+                requestEntity.setContent(content);
+                requestEntity.addHeader("token", UserUtils.getToken());
+                requestEntity.addHeader("sign", EncodeHelper.getSign(content));
+                RequestBuilder.get().send(requestEntity, new RequestCallback<String>() {
+                    @Override
+                    public void onStart() {
+                    }
+
+                    @Override
+                    public void onSuccess(int code, String object) {
+                        ToastUtils.show("提交成功");
+                        finish();
+                    }
+
+                    @Override
+                    public void onFailure(int code, String msg) {
+                        ToastUtils.show(msg);
+                    }
+                });
             }
         });
     }
