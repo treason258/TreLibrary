7fab8df3ca12db3970d99ffe81baea3785a2de3b
matengfei <matengfei@xin.com>
2018-11-07 18:23:56 +0800

    LovelyReaderAS - 首页读书相关代码整理

--- LovelyReaderAS/app/src/main/java/com/haoyang/lovelyreader/tre/ui/frgament/HomeFragment.java
+++ LovelyReaderAS/app/src/main/java/com/haoyang/lovelyreader/tre/ui/frgament/HomeFragment.java
@@ -4,13 +4,11 @@ import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,10 +18,8 @@ import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.app.base.common.util.Size;
 import com.haoyang.lovelyreader.R;
 import com.haoyang.lovelyreader.tre.base.BaseFragment;
 import com.haoyang.lovelyreader.tre.bean.BookBean;
@@ -38,6 +34,7 @@ import com.haoyang.lovelyreader.tre.helper.Configs;
 import com.haoyang.lovelyreader.tre.helper.DBHelper;
 import com.haoyang.lovelyreader.tre.helper.EncodeHelper;
 import com.haoyang.lovelyreader.tre.helper.OnBookAddEvent;
+import com.haoyang.lovelyreader.tre.helper.ReaderHelper;
 import com.haoyang.lovelyreader.tre.helper.UrlConfig;
 import com.haoyang.lovelyreader.tre.http.MyRequestEntity;
 import com.haoyang.lovelyreader.tre.ui.FileActivity;
@@ -48,16 +45,7 @@ import com.haoyang.lovelyreader.tre.util.Utils;
 import com.haoyang.lovelyreader.tre.wifi.Constants;
 import com.haoyang.lovelyreader.tre.wifi.PopupMenuDialog;
 import com.haoyang.lovelyreader.tre.wifi.WebService;
-import com.haoyang.reader.sdk.AnimationType;
 import com.haoyang.reader.sdk.Book;
-import com.haoyang.reader.sdk.ColorService;
-import com.haoyang.reader.sdk.DefaultCustomServivce;
-import com.haoyang.reader.sdk.FeedBack;
-import com.haoyang.reader.sdk.LineSpace;
-import com.haoyang.reader.sdk.PageStyle;
-import com.haoyang.reader.sdk.ReaderSDK;
-import com.haoyang.reader.sdk.SDKParameterInfo;
-import com.haoyang.reader.sdk.ShareEntity;
 import com.haoyang.reader.service.bookservice.BookInfoService;
 import com.hwangjr.rxbus.RxBus;
 import com.hwangjr.rxbus.annotation.Subscribe;
@@ -70,7 +58,6 @@ import com.mjiayou.trecorelib.dialog.TCAlertDialog;
 import com.mjiayou.trecorelib.event.UserLoginStatusEvent;
 import com.mjiayou.trecorelib.http.RequestSender;
 import com.mjiayou.trecorelib.http.callback.RequestCallback;
-import com.mjiayou.trecorelib.json.JsonParser;
 import com.mjiayou.trecorelib.util.ConvertUtils;
 import com.mjiayou.trecorelib.util.LogUtils;
 import com.mjiayou.trecorelib.util.ToastUtils;
@@ -237,7 +224,7 @@ public class HomeFragment extends BaseFragment {
                 book.bookCover = bookBean.getLocalCoverPath();
 
                 ToastUtils.show("正在打开书籍...");
-                startReader(book, mUserBean);
+                ReaderHelper.startReader(mActivity, book, mUserBean);
             }
         });
         gvBook.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
@@ -420,219 +407,6 @@ public class HomeFragment extends BaseFragment {
         }
     }
 
-    /**
-     * 启动阅读器进行阅读
-     */
-    private void startReader(Book book, UserBean userBean) {
-        try {
-            // parameter
-            SDKParameterInfo parameter = new SDKParameterInfo();
-            parameter.appId = "773278";
-            parameter.appKey = "10f2a8b3759b4304a5414269c5c4bf63";
-            parameter.userId = userBean.getUid(); // 接入时需提供应用下唯一的，否则会出现数据错乱。
-            parameter.userName = userBean.getNickName();
-
-            ReaderSDK readerSDK = ReaderSDK.getInstance();
-            readerSDK.initSDK(mContext, parameter);
-            readerSDK.setCurrentPageStyle("wallpapers/background_4.jpg"); // 设置默认的样式，这个方法需要在setPageStyle方法之前执行
-            readerSDK.setPageStyle(getPageStyleList()); // 将样式对象列表传给阅读器，阅读器根据这个来定义背景切换，如果在之前没有执行setCurrentPageStyle这个方法，setPageStyle就会把第一个做为默认样式
-            readerSDK.setAnimationType(AnimationType.SlideAnimation); // 设置默认翻页动画。
-            readerSDK.setAnimationSpeed(10); // 设置翻页动画移动时，每次移动的距离基础数字。
-            readerSDK.setAnimationSpeedFactor(1.2f); // 设置翻页动画速度不断加快的因子。
-            readerSDK.setTypefaceSwitchEnable(true); // 是否开启字体切换功能。
-            readerSDK.setDefaultTypeface(Typeface.createFromAsset(mContext.getAssets(), "yahei.ttf")); // 设置当前的默认字体。
-
-            // 设置阅读的各种属性
-            readerSDK.setUpDownSpace(100); // 设置阅读器上下屏幕边与文字之间的间距。
-            readerSDK.setLeftRightSpace(50); // 设置阅读器左右屏幕边与文字之间的间距。
-            readerSDK.setLineSpacing(LineSpace.lineSpaceMiddle); // 设置当前行间距。
-            readerSDK.setStressLineThickness(3); // 设置对文字画线时线条的粗细程度。
-            readerSDK.setStressLineColor(new ColorService(230, 45, 150)); // 设置对文字画线时线条的颜色。
-            readerSDK.setShowCatalog(); // 设置阅读器显示目录。setHideCatalog
-            readerSDK.setShowBattery(true); // 设置阅读器显示电池。
-            readerSDK.setShowPageNumber(true); // 设置阅读器显示页码。
-            readerSDK.setShowTime(true); // 设置阅读器显示当前时间。
-            readerSDK.setShareEnable(false); // 是否需要分享功能。
-            readerSDK.setTypefaceSwitchEnable(false); // 是否需要有切换字体功能。
-
-            // 设置阅读器内部一些功能由开发者来实现的接口，这里需要开发者实现SDKInteractive接口。
-            readerSDK.setSDKInteractive(new SDKInteractiveImpl());
-            readerSDK.setCustomServivce(new CustomServiceImpl());
-
-            String id = book.path;
-            if (book.path != null) {
-                int index = book.path.lastIndexOf(File.separator);
-                if (index != -1) {
-                    id = book.path.substring(index + 1);
-                }
-            }
-            book.id = id;
-
-            readerSDK.startReader(mActivity, book, ReaderSDK.FilePathType.Local_File);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * 图片处理方式，开发者自定义。
-     *
-     * @author tianyu912@yeah.net Mar 10, 2018
-     */
-    private class CustomServiceImpl extends DefaultCustomServivce {
-        @Override
-        public Size getImageSize(float imageWidth, float imageHeight, float readAdeaWidth, float readAreaHeight) {
-            float height, width;
-            float unit = readAdeaWidth / 4.0f;
-            if (imageWidth <= unit) {
-                width = unit;
-                float rate = width / imageWidth;
-                height = rate * imageHeight;
-            } else {
-                width = readAdeaWidth;
-                float rate = width / imageWidth;
-                height = rate * imageHeight;
-            }
-
-            if (height > readAreaHeight) {
-                float rate = readAreaHeight / height;
-                height = (int) (rate * height);
-                width = width * rate;
-            }
-            return new Size((int) width, (int) height);
-        }
-    }
-
-    /**
-     * SDKInteractiveImpl
-     */
-    private class SDKInteractiveImpl implements ReaderSDK.SDKInteractive {
-
-        @Override
-        public void share(ShareEntity shareEntity) { // 打开分享界面
-            ToastUtils.show("分享");
-        }
-
-        @Override
-        public void typefaceSwitch() { // 打开切换字体界面
-            Log.d(TAG, "typefaceSwitch() called");
-            ToastUtils.show("切换字体");
-        }
-
-        @Override
-        public void toolsBar(RelativeLayout relativeLayout) {
-            View contentView = LayoutInflater.from(mContext).inflate(R.layout.layout_reader_custom, null);
-            relativeLayout.addView(contentView);
-
-            // findViewById
-            View tvComment = contentView.findViewById(R.id.tvComment);
-            View tvBuy = contentView.findViewById(R.id.tvBuy);
-
-            // tvComment
-            tvComment.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    ToastUtils.show("评论");
-                }
-            });
-            // tvBuy
-            tvBuy.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    ToastUtils.show("购买");
-                }
-            });
-        }
-
-        @Override
-        public void feedback(FeedBack feedBack) {
-            LogUtils.d(TAG, "feedback() called with: feedBack = [" + JsonParser.get().toJson(feedBack) + "]");
-            ToastUtils.show("意见反馈");
-        }
-
-        @Override
-        public void decrypt(String sourceFilePath, String targetFilePath) {
-            LogUtils.d(TAG, "decrypt() called with: sourceFilePath = [" + sourceFilePath + "], targetFilePath = [" + targetFilePath + "]");
-            ToastUtils.show("decrypt");
-        }
-    }
-
-    /**
-     * getPageStyleList
-     */
-    private List<PageStyle> getPageStyleList() {
-        String[] wordColorArray = {
-                "0x372d21",
-                "0xb6bbbe",
-                "0x322b23",
-                "0x444444",
-                "0x444444",
-                "0x444444",
-                "0x444444",
-                "0x342d25",
-                "0x293a27",
-                "0x000200",
-                "0x5f707a",
-                "0x96938e"}; // 阅读器文字颜色，与背景资源是一一对应的。
-
-        String[] wallPaperArray = {
-                "wallpapers/background_2.jpg",
-                "wallpapers/background_1.jpg",
-                "wallpapers/background_3.jpg",
-                "wallpapers/background_4.jpg",
-                "wallpapers/background_5.jpg",
-                "wallpapers/background_6.jpg",
-                "wallpapers/background_7.jpg",
-                "0xf6efe7",
-                "0xceefd0",
-                "0x596476",
-                "0x001d28",
-                "0x39312f"}; // 阅读器背景资源。
-
-        PageStyle.BackgroundFillMode[] wallPaperType = {
-                PageStyle.BackgroundFillMode.fullscreen,
-                PageStyle.BackgroundFillMode.tileMirror,
-                PageStyle.BackgroundFillMode.fullscreen,
-                PageStyle.BackgroundFillMode.fullscreen,
-                PageStyle.BackgroundFillMode.fullscreen,
-                PageStyle.BackgroundFillMode.fullscreen,
-                PageStyle.BackgroundFillMode.fullscreen,
-                PageStyle.BackgroundFillMode.backgroundColor,
-                PageStyle.BackgroundFillMode.backgroundColor,
-                PageStyle.BackgroundFillMode.backgroundColor,
-                PageStyle.BackgroundFillMode.backgroundColor,
-                PageStyle.BackgroundFillMode.backgroundColor}; // 阅读器背景资源类型。
-
-        PageStyle.NightDayMode[] nightDayMode = {
-                PageStyle.NightDayMode.day,
-                PageStyle.NightDayMode.none,
-                PageStyle.NightDayMode.none,
-                PageStyle.NightDayMode.none,
-                PageStyle.NightDayMode.none,
-                PageStyle.NightDayMode.none,
-                PageStyle.NightDayMode.none,
-                PageStyle.NightDayMode.none,
-                PageStyle.NightDayMode.none,
-                PageStyle.NightDayMode.none,
-                PageStyle.NightDayMode.none,
-                PageStyle.NightDayMode.night}; // 定义每种背景及文字是夜间模式还是白天模式。
-
-        List<PageStyle> pageStyleList = new ArrayList<>();
-        for (int index = 0; index < wallPaperArray.length; index++) { // 生成样式对象。
-            PageStyle pageStyle = new PageStyle();
-            pageStyle.styleName = wallPaperArray[index];
-            pageStyle.backgroundValue = wallPaperArray[index];
-            pageStyle.backgroundFillMode = wallPaperType[index];
-            pageStyle.textColor = wordColorArray[index];
-            pageStyle.catalogColor = wordColorArray[index];
-            pageStyle.pageNumberColor = wordColorArray[index];
-            pageStyle.timeColor = wordColorArray[index];
-            pageStyle.nightDayMode = nightDayMode[index];
-            pageStyle.source = PageStyle.Source.DEVELOPER;
-            pageStyleList.add(pageStyle);
-        }
-        return pageStyleList;
-    }
 
     /**
      * syncGuestBook
