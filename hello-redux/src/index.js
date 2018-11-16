3f616aab726003d0f93291b3807bf099480fd35b
matengfei <matengfei@xin.com>
2018-11-16 15:34:01 +0800

    hello-redux - 学习：动手实现 Redux（六）：Redux 总结

--- hello-redux/src/index.js
+++ hello-redux/src/index.js
@@ -11,49 +11,86 @@
 // // Learn more about service workers: http://bit.ly/CRA-PWA
 // serviceWorker.unregister();
 
-
-let appState = {
-    title: {
-        text: 'React.js 小书',
-        color: 'red',
-    },
-    content: {
-        text: 'React.js 小书内容',
-        color: 'blue'
-    }
+function renderApp(newAppState, oldAppState = {}) {
+    if (newAppState === oldAppState) return // 数据没有变化就不渲染了
+    console.log('render app...')
+    renderTitle(newAppState.title, oldAppState.title)
+    renderContent(newAppState.content, oldAppState.content)
 }
 
-function dispatch (action) {
+function renderTitle(newTitle, oldTitle = {}) {
+    if (newTitle === oldTitle) return // 数据没有变化就不渲染了
+    console.log('render title...')
+    const titleDOM = document.getElementById('title')
+    titleDOM.innerHTML = newTitle.text
+    titleDOM.style.color = newTitle.color
+}
+
+function renderContent(newContent, oldContent = {}) {
+    if (newContent === oldContent) return // 数据没有变化就不渲染了
+    console.log('render content...')
+    const contentDOM = document.getElementById('content')
+    contentDOM.innerHTML = newContent.text
+    contentDOM.style.color = newContent.color
+}
+
+function createStore(stateChanger) {
+    let state = null
+    const listeners = []
+    const subscribe = (listener) => listeners.push(listener)
+    const getState = () => state
+    const dispatch = (action) => {
+        state = stateChanger(state, action)
+        listeners.forEach((listener) => listener())
+    }
+    dispatch({})  // 初始化 state
+    return {getState, dispatch, subscribe}
+}
+
+function reducer(state, action) {
+    if (!state) {
+        return {
+            title: {
+                text: 'React.js 小书',
+                color: 'red',
+            },
+            content: {
+                text: 'React.js 小书内容',
+                color: 'blue'
+            }
+        }
+    }
     switch (action.type) {
         case 'UPDATE_TITLE_TEXT':
-            appState.title.text = action.text
-            break
+            return { // 构建新的对象并且返回
+                ...state,
+                title: {
+                    ...state.title,
+                    text: action.text
+                }
+            }
+
         case 'UPDATE_TITLE_COLOR':
-            appState.title.color = action.color
-            break
+            return { // 构建新的对象并且返回
+                ...state,
+                title: {
+                    ...state.title,
+                    color: action.color
+                }
+            }
         default:
-            break
+            return state
     }
 }
 
-function renderApp (appState) {
-    renderTitle(appState.title)
-    renderContent(appState.content)
-}
+const store = createStore(reducer)
+let oldState = store.getState() // 缓存旧的 state
+store.subscribe(() => {
+    const newState = store.getState() // 数据可能变化，获取新的 state
+    renderApp(newState, oldState) // 把新旧的 state 传进去渲染
+    oldState = newState // 渲染完以后，新的 newState 变成了旧的 oldState，等待下一次数据变化重新渲染
+})
 
-function renderTitle (title) {
-    const titleDOM = document.getElementById('title')
-    titleDOM.innerHTML = title.text
-    titleDOM.style.color = title.color
-}
-
-function renderContent (content) {
-    const contentDOM = document.getElementById('content')
-    contentDOM.innerHTML = content.text
-    contentDOM.style.color = content.color
-}
-
-renderApp(appState) // 首次渲染页面
-dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }) // 修改标题文本
-dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' }) // 修改标题颜色
-renderApp(appState) // 把新的数据渲染到页面上
+renderApp(store.getState()) // 首次渲染页面
+store.dispatch({type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》'}) // 修改标题文本
+store.dispatch({type: 'UPDATE_TITLE_COLOR', color: 'grey'}) // 修改标题颜色
