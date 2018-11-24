d0458be373eb52e9fb6b457f5574dd37eba1cc77
matengfei <ma.tengfei2008@163.com>
2018-11-25 00:59:24 +0800

    LovelyReaderAS - add XDrawerLayout

---
+++ LovelyReaderAS/app/src/main/java/com/haoyang/lovelyreader/tre/widget/XDrawerLayoutImpl.java
@@ -0,0 +1,9 @@
+package com.haoyang.lovelyreader.tre.widget;
+
+/**
+ * Created by treason on 2018/11/25.
+ */
+
+interface XDrawerLayoutImpl {
+    void setChildInsets(Object insets, boolean drawStatusBar);
+}
