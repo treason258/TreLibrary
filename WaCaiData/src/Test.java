0aa6ddc7ceaf7a8eddaea1a3e47297728c1d43c0
matengfei <matengfei@xin.com>
2018-12-21 14:21:46 +0800

    WaCaiData - new project

---
+++ WaCaiData/src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+
+    private static String TAG = Test.class.toString();
+
+    public static void main(String[] args) {
+        System.out.println("--------------------------------");
+        System.out.println(TAG + " | " + "main");
+        System.out.println("--------------------------------");
+    }
+}
