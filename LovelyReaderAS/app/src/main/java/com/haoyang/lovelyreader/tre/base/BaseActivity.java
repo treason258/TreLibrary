e7038cca11a6291380b8ac7b9f522f824084e094
matengfei <matengfei@xin.com>
2019-01-04 19:02:29 +0800

    LovelyReader - 配合TreCore修改

--- LovelyReaderAS/app/src/main/java/com/haoyang/lovelyreader/tre/base/BaseActivity.java
+++ LovelyReaderAS/app/src/main/java/com/haoyang/lovelyreader/tre/base/BaseActivity.java
@@ -7,4 +7,9 @@ import com.mjiayou.trecorelib.base.TCActivity;
  */
 
 public abstract class BaseActivity extends TCActivity {
+
+    @Override
+    protected boolean checkHideTitleBar() {
+        return true;
+    }
 }
