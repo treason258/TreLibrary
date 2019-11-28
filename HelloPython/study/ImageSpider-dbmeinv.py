196c859a009cdc74f514c0c8ab7a409b756de1dc
matengfei <matengfei@xin.com>
2019-11-28 17:11:33 +0800

    HelloPython - ImageSpider-dbmeinv 利用SpiderUtils整理

--- HelloPython/study/ImageSpider-dbmeinv.py
+++ HelloPython/study/ImageSpider-dbmeinv.py
@@ -1,13 +1,6 @@
 # -*- coding:utf-8 -*-
-import urllib
-import urllib2
 import re
-import os
-import time
-
-import ssl
-
-ssl._create_default_https_context = ssl._create_unverified_context
+import SpiderUtils
 
 
 class ImageSpider(object):
@@ -16,53 +9,28 @@ class ImageSpider(object):
     def __init__(self):
         pass
 
-    def saveImage(self, imageUrl, imagePath):
-        try:
-            request = urllib2.Request(imageUrl, headers=headers)
-            imageData = urllib2.urlopen(request).read()
-            with open(imagePath, "wb") as f:
-                f.write(imageData)
-            time.sleep(0.1)
-        except BaseException:
-            print '下载失败 | imagePath = ' + imagePath + " | imageUrl = " + imageUrl
-        else:
-            print '下载成功 | imagePath = ' + imagePath + " | imageUrl = " + imageUrl
-
     def getImageFormUrl(self, url):
         print "--------------------------------解析网页代码"
-        print "网页地址 | url = " + url
-        request = urllib2.Request(url, headers=headers)
-        response = urllib2.urlopen(request)
-        text = response.read()
-        print "打印网页内容"
-        print text
-        patternStr = r"(?<=onerror=\"img_error\(this\);\" src=\").+?\.jpg(?=\" referrerpolicy=\"no-referrer\")"
-        pattern = re.compile(patternStr)
-        imageUrls = pattern.findall(text)
+        print "--网页地址 | url = " + url
+        content = SpiderUtils.getHtmlContent(url)
+        # print "--打印网页内容"
+        # print content
+
+        imageUrls = SpiderUtils.findTextArray(content, re.compile(r"(?<=onerror=\"img_error\(this\);\" src=\").+?\.jpg(?=\" referrerpolicy=\"no-referrer\")"))
         print "打印图片列表"
         print imageUrls
         for imageUrl in imageUrls:
             imagePath = imageDir + ("%d.jpg" % ImageSpider.num)
-            self.saveImage(imageUrl, imagePath)
+            SpiderUtils.saveImage(imageUrl, imagePath)
             ImageSpider.num += 1
 
 
 if __name__ == '__main__':
 
-    print "--------------------------------项目配置信息"
-    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"}
-    homeDir = os.environ['HOME']
-    imageDir = homeDir + '/Downloads/python/ImageSpider-dbmeinv/'
-    print "用户目录 | home_dir = " + str(homeDir)
-    print "下载目录 | imageDir = " + str(imageDir)
-
     print "--------------------------------创建下载目录"
-    isdir = os.path.isdir(imageDir)
-    if isdir:
-        print "下载目录已存在，不需要创建"
-    else:
-        print "下载目录不存在，开始创建"
-        os.makedirs(imageDir)
+    imageDir = SpiderUtils.getHomeDir() + '/Downloads/python/ImageSpider-dbmeinv/'
+    print "--下载目录 | imageDir = " + str(imageDir)
+    SpiderUtils.makeDirs(imageDir)
 
     print "--------------------------------输入页码"
     pageFrom = raw_input("输入开始页：")
