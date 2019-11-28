801d70a6226ee7957a5bc277109a69025abf43d6
matengfei <matengfei@xin.com>
2019-11-28 17:02:49 +0800

    HelloPython - ImageSpider-8630vip 批量下载

--- HelloPython/study/ImageSpider-8630vip.py
+++ HelloPython/study/ImageSpider-8630vip.py
@@ -1,89 +1,45 @@
 # -*- coding:utf-8 -*-
-import sys
-import urllib2
 import re
-import os
-import time
-
-import ssl
-
-ssl._create_default_https_context = ssl._create_unverified_context
+import SpiderUtils
 
 
-class ImageSpider(object):
-
-    def __init__(self):
-        pass
-
-    def saveImage(self, imageUrl, imagePath):
-        try:
-            request = urllib2.Request(imageUrl, headers=headers)
-            imageData = urllib2.urlopen(request).read()
-            with open(imagePath, "wb") as f:
-                f.write(imageData)
-            time.sleep(0.1)
-        except BaseException:
-            print '下载失败 | imagePath = ' + imagePath + " | imageUrl = " + imageUrl
-        else:
-            print '下载成功 | imagePath = ' + imagePath + " | imageUrl = " + imageUrl
-
-
-if __name__ == '__main__':
-
-    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"}
-
+def get8630VipImage(url):
     print "--------------------------------解析网页代码"
-    url = "https://yy.8630vip.com/home/book/capter/id/28514"
-    print "网页地址 | url = " + url
-    request = urllib2.Request(url, headers=headers)
-    response = urllib2.urlopen(request)
-    text = response.read()
-    print "----打印网页内容"
-    print text
+    print "--网页地址 | url = " + url
+    content = SpiderUtils.getHtmlContent(url)
+    # print "--打印网页内容"
+    # print content
 
-    patternTitle = re.compile(r"(?<=<div class=\"title\">).+?(?=<span class=\"subtitle\">)")
-    titleArray = patternTitle.findall(text)
-    title = titleArray[0].decode('utf-8')
-    print "----打印标题"
-    print title
-
-    patternSubtitle = re.compile(r"(?<=<span class=\"subtitle\">).+?(?=</span>)")
-    subtitleArray = patternSubtitle.findall(text)
-    subtitle = subtitleArray[0].decode('utf-8')
-    print "----打印副标题"
-    print subtitle
-
-    titleAll = title + "-" + subtitle + "/"
-    print titleAll
-
-    print "--------------------------------项目配置信息"
-    reload(sys)
-    sys.setdefaultencoding("utf-8")
-    homeDir = os.environ['HOME']
-    imageDir = homeDir + '/Downloads/python/ImageSpider-8630vip/' + titleAll
-    print "用户目录 | home_dir = " + str(homeDir)
-    print "下载目录 | imageDir = " + str(imageDir)
+    print "--------------------------------获得文件夹名字"
+    title1 = SpiderUtils.findText(content, re.compile(r"(?<=<div class=\"title\">).+?(?=<span class=\"subtitle\">)"))
+    title2 = SpiderUtils.findText(content, re.compile(r"(?<=<span class=\"subtitle\">).+?(?=</span>)"))
+    folderName = title1 + "-" + title2 + "/"
+    print "--打印标题1 | title1 = " + str(title1)
+    print "--打印标题2 | title2 = " + str(title2)
+    print "--打印文件夹名称 | folderName = " + folderName
 
     print "--------------------------------创建下载目录"
-    isdir = os.path.isdir(imageDir)
-    if isdir:
-        print "下载目录已存在，不需要创建"
-    else:
-        print "下载目录不存在，开始创建"
-        os.makedirs(imageDir)
+    imageDir = SpiderUtils.getHomeDir() + '/Downloads/python/ImageSpider-8630vip/' + folderName
+    print "--下载目录 | imageDir = " + str(imageDir)
+    SpiderUtils.makeDirs(imageDir)
 
-    # pattern = re.compile(r"(?<=888888).+?(?=888888)")
-    # pattern = re.compile(r"(?<=img src=\").+?\.jpg(?=\" alt=\"图)")
-    patternImages = re.compile(r"(?<=img src=\").+?(?=1.jpg\" alt=\"图)")
-    imageUrlArray = patternImages.findall(text)
+    print "--------------------------------解析图片地址"
+    imageUrlArray = SpiderUtils.findTextArray(content, re.compile(r"(?<=img src=\").+?(?=1.jpg\" alt=\"图)"))
     print "----打印图片列表"
     print imageUrlArray
 
     imageUrlPrefix = imageUrlArray[0]
     print "imageUrlPrefix = " + imageUrlPrefix
 
-    imageSpider = ImageSpider()
-    for i in range(1, 40):
+    for i in range(1, 3):
         imageUrl = imageUrlPrefix + ("%d.jpg" % i)
         imagePath = imageDir + ("%d.jpg" % i)
-        imageSpider.saveImage(imageUrl, imagePath)
+        SpiderUtils.saveImage(imageUrl, imagePath)
+
+
+if __name__ == '__main__':
+    urlPrefix = "https://yy.8630vip.com/home/book/capter/id/"
+    beginIndex = 28500
+    for i in range(1, 5):
+        url = urlPrefix + str(beginIndex + i)
+        get8630VipImage(url)
---
+++ HelloPython/study/SpiderUtils.py
@@ -0,0 +1,85 @@
+# -*- coding:utf-8 -*-
+
+import sys
+import os
+import re
+
+# pattern
+pattern_default = re.compile(r"(?<=888888).+?(?=888888)")
+
+# headers
+headers_chrome_old = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"}
+headers_chrome_new = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"}
+headers_ios = {"User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"}
+
+
+def findTextArray(content, pattern):
+    array = pattern.findall(content)
+    return array
+
+
+# 利用正则查找字符串
+def findText(content, pattern):
+    textArray = pattern.findall(content)
+    text = textArray[0].decode('utf-8')
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
+    return text
+
+
+# 利用正则查找字符串
+def findTextByStr(content, patternStr, flags=0):
+    pattern = re.compile(patternStr)
+    textArray = pattern.findall(content)
+    text = textArray[0].decode('utf-8')
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
+    return text
+
+
+# 获取用户根目录 eg /Users/xin
+def getHomeDir():
+    homeDir = os.environ['HOME']
+    return homeDir
+
+
+# 创建目录
+def makeDirs(dir):
+    isdir = os.path.isdir(dir)
+    if isdir:
+        print "目录已存在，不需要创建 | dir = " + dir
+    else:
+        print "目录不存在，创建目录 | dir = " + dir
+        os.makedirs(dir)
+
+
+# 获取网页内容
+def getHtmlContent(url):
+    import urllib2
+    import ssl
+    ssl._create_default_https_context = ssl._create_unverified_context
+    try:
+        request = urllib2.Request(url=url, headers=headers_ios)
+        response = urllib2.urlopen(request)
+        content = response.read()
+    except urllib2.HTTPError:
+        print '获取网页内容失败 | url = ' + url
+        return ""
+    else:
+        return content
+
+
+# 下载保存图片
+def saveImage(imageUrl, imagePath):
+    import urllib2
+    import time
+    try:
+        request = urllib2.Request(url=imageUrl, headers=headers_ios)
+        response = urllib2.urlopen(request).read()
+        with open(imagePath, "wb") as imageFile:
+            imageFile.write(response)
+        time.sleep(0.2)
+    except urllib2.HTTPError:
+        print '下载保存图片失败 | imageUrl = ' + imageUrl
+    else:
+        print '下载保存图片成功 | imageUrl = ' + imageUrl + " | imagePath = " + imagePath
